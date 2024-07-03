4225f412304dbf3b2abb8a532726c1631d9e6655
noodelz <adrianeroa081202@gmail.com>
2024-07-03 08:46:53 +0800

    fixed address on prod

--- components/Map.vue
+++ components/Map.vue
@@ -142,10 +142,6 @@ const onClickOriginPin = async () => {
             async (position) => {
                 const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.coords.latitude},${position.coords.longitude}&key=${config.public.GCP_API_KEY}`)
                 const data = await response.json()
-
-                console.log(response.ok)
-                console.log(data)
-                console.log(data.results)
                 
                 if (response.ok)
                     origin.value = data.results[0].formatted_address
